e91a58063dc67d405fd2c46ec54378646c2ac540
GuyMAUR <147267615+GuyMAUR@users.noreply.github.com>
2023-12-24 02:09:25 +0100

    Add files via upload

---
+++ POUFF/HTML/bet.js
@@ -0,0 +1,15 @@
+//  var titre=document.getElementById("titre")
+//  titre.textContent="t'es ou, trouve moi en piss"
+//  titre.style.color="red"
+//  var serieux=document.getElementById("serieux")
+
+
+
+//  serieux.addEventListener("input",function(){
+    //  titre.textContent = serieux.value.toUpperCase()
+    //  titre.style.fontSize="80px"
+    //  titre.style.color="blue"
+//  })
+
+
+
